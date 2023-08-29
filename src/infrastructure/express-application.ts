4816d1986ab8903f5a12728018c3fa84987cc82c
Pierre Viara <pierre.viara@outlook.com>
2023-08-29 16:33:54 +0200

    feat(express-app): add  environment configuration

--- src/infrastructure/express-application.ts
+++ src/infrastructure/express-application.ts
@@ -1,9 +1,32 @@
 import { ExpressServer } from './express-server';
+import * as dotenv from 'dotenv';
 
 export class ExpressApplication {
-    private server = new ExpressServer('3000');
+    private server: ExpressServer;
+
+    constructor() {
+        this.configureEnvironment();
+
+        const port = this.getPort();
+        this.server = new ExpressServer(port);
+    }
 
     bootstrap(): void {
         this.server.bootstrap();
+    }
+
+    private configureEnvironment(): void {
+        dotenv.config({
+            path: '.env',
+        });
+    }
+
+    private getPort(): string {
+        const port = process.env.PORT;
+        if (!port) {
+            throw new Error('No port was found in env file.');
+        }
+
+        return port;
     }
 }
