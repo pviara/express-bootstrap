0e550ab118421f6a6a6f9fd5e43df0f0f51e802e
Pierre Viara <pierre.viara@outlook.com>
2023-09-14 18:10:27 +0200

    feat(user): add user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { User } from './user';
+import { UserService } from './user.service';
+
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    add(username: string): User {
+        // is the username empty ?
+        // is the username whitespaced ?
+        // other checks...
+        return this.userService.add(username);
+    }
+
+    getById(id: number): User | null {
+        // is the id a decimal ?
+        // is the id a negative number ?
+        // other checks...
+        return this.userService.getById(id);
+    }
+}
--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -5,6 +5,7 @@ export class UserJSONService implements UserService {
     add(username: string): User {
         throw new Error('Method not implemented.');
     }
+
     getById(id: number): User | null {
         throw new Error('Method not implemented.');
     }
