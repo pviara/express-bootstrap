654421f7cfed54cbbb0b3124736e1c94b18a6e4e
Pierre Viara <pierre.viara@outlook.com>
2023-09-21 13:24:34 +0200

    feat(user): implement user service

--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -1,32 +1,58 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { isArrayEmpty } from '../utils';
 import { User } from './user';
 import { UserService } from './user.service';
+
+const DEFAULT_USER_ID = 0;
 
 export class UserJSONService implements UserService {
     private readonly userJsonPath = './src/user/users.json';
 
+    constructor() {
+        this.writeDefaultUsersJsonFile();
+    }
+
     add(username: string): User {
-        const buffer = readFileSync(this.userJsonPath);
-        const users = JSON.parse(buffer.toString()) as User[];
+        const users = this.getUsersFromJsonFile();
 
-        const newUser = new User(0, username);
+        const newId = this.generateUniqueId(users);
+        const newUser = new User(newId, username);
+
         users.push(newUser);
-
-        writeFileSync(this.userJsonPath, JSON.stringify(users));
+        this.overrideUsers(users);
 
         return newUser;
     }
 
     getById(id: number): User | null {
-        // 1. de récupérer le fichier json et le lire
-        // 2. parcourir les users et de trouver celui qui
-        // possède l'id reçu en paramètre (id)
-        // 3. retourner l'utilisateur s'il a été trouvé, sinon de retourner null
-
-        const buffer = readFileSync(this.userJsonPath);
-        const users = JSON.parse(buffer.toString()) as User[];
+        const users = this.getUsersFromJsonFile();
 
         const existingUser = users.find((user) => user.id == id);
         return existingUser || null;
+    }
+
+    private writeDefaultUsersJsonFile(): void {
+        if (!existsSync(this.userJsonPath)) {
+            writeFileSync(this.userJsonPath, JSON.stringify([]));
+        }
+    }
+
+    private getUsersFromJsonFile(): User[] {
+        const buffer = readFileSync(this.userJsonPath);
+        const users = JSON.parse(buffer.toString()) as User[];
+        return users;
+    }
+
+    private generateUniqueId(users: User[]): number {
+        if (isArrayEmpty(users)) {
+            return DEFAULT_USER_ID;
+        }
+
+        const userIds = users.map((user) => user.id);
+        return Math.max(...userIds) + 1;
+    }
+
+    private overrideUsers(users: User[]): void {
+        writeFileSync(this.userJsonPath, JSON.stringify(users));
     }
 }
