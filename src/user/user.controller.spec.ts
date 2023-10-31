34ddb353fc5e0d3f8f9fed79be328d5351987534
Pierre Viara <pierre.viara@outlook.com>
2023-10-31 12:57:41 +0100

    feat(user): add checks inside controller methods

---
+++ src/user/user.controller.spec.ts
@@ -0,0 +1,86 @@
+import { User } from './user';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+    let sut: UserController;
+    let userService: UserServiceSpy;
+
+    beforeEach(() => {
+        userService = new UserServiceSpy();
+        sut = new UserController(userService);
+    });
+
+    describe('add', () => {
+        it('should throw an error when given username is empty', () => {
+            const usernames: string[] = ['', ' ', '     '];
+
+            for (const username of usernames) {
+                expect(() => sut.add(username)).toThrow();
+            }
+        });
+
+        it('should call add from UserService when given username is valid', () => {
+            const validUsernames: string[] = ['username_1', 'us', 'test'];
+
+            for (const validUsername of validUsernames) {
+                sut.add(validUsername);
+            }
+
+            expect(userService.callsToAdd).toBe(validUsernames.length);
+        });
+    });
+
+    describe('getById', () => {
+        it('should throw an error when given id is not a number', () => {
+            const ids = [NaN, '28', ''];
+
+            for (const id of ids) {
+                expect(() => sut.getById(id as number)).toThrow();
+            }
+        });
+
+        it('should throw an error when given id is a decimal', () => {
+            const ids: number[] = [0.4, 11.993, 2.7];
+
+            for (const id of ids) {
+                expect(() => sut.getById(id)).toThrow();
+            }
+        });
+
+        it('should throw an error when given id is negative', () => {
+            const ids: number[] = [-1, -3, -482];
+
+            for (const id of ids) {
+                expect(() => sut.getById(id)).toThrow();
+            }
+        });
+
+        it('should call getById on UserService when given id is valid', () => {
+            const validIds: number[] = [0, 31, 838, 4];
+
+            for (const validId of validIds) {
+                sut.getById(validId);
+            }
+
+            expect(userService.callsToGetById).toBe(validIds.length);
+        });
+    });
+});
+
+class UserServiceSpy implements UserService {
+    callsToAdd = 0;
+    callsToGetById = 0;
+
+    private dummyUser = new User(0, '');
+
+    add(username: string): User {
+        this.callsToAdd++;
+        return this.dummyUser;
+    }
+
+    getById(id: number): User | null {
+        this.callsToGetById++;
+        return this.dummyUser;
+    }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,9 @@
+import {
+    isNumberDecimal,
+    isNumberNegative,
+    isStrictlyNaN,
+    isStringEmpty,
+} from '../utils';
 import { User } from './user';
 import { UserService } from './user.service';
 
@@ -5,16 +11,26 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     add(username: string): User {
-        // is the username empty ?
-        // is the username whitespaced ?
-        // other checks...
+        if (isStringEmpty(username)) {
+            throw new Error('Given username is empty.');
+        }
+
         return this.userService.add(username);
     }
 
     getById(id: number): User | null {
-        // is the id a decimal ?
-        // is the id a negative number ?
-        // other checks...
+        if (isStrictlyNaN(id)) {
+            throw new Error('Given id is not a number.');
+        }
+
+        if (isNumberDecimal(id)) {
+            throw new Error('Given id is a decimal.');
+        }
+
+        if (isNumberNegative(id)) {
+            throw new Error('Given id is negative.');
+        }
+
         return this.userService.getById(id);
     }
 }
